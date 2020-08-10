41dda3e4b1d352dc9906cf990bb855026d953509
itsOrD <matthew.beckerleg@gmail.com>
2020-08-10 15:55:49 -0400

    basic input with f-strings

---
+++ 08Aug/inputb4lunch.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+''' itsOrD | practicing taking user input '''
+
+def main():
+    '''main point of file'''
+    name = input(f'''What's your name? ''')
+    print(f'''Hi there {name.capitalize()}!''')
+
+if __name__ == "__main__":
+    main()
