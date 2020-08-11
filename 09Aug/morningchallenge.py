87bdcef9eef3d666fbdf075a81e847b3e878fec9
itsOrD <matthew.beckerleg@gmail.com>
2020-08-11 11:03:33 -0400

    review of list and input printing

---
+++ 09Aug/morningchallenge.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+''' itsOrD || practicing with Python basics '''
+
+def main():
+    '''salty meme'''
+    icecream = ['flavors', 'salty']
+    icecream.append(99)
+    name = input(f'''What's your name? ''')
+    print(f'''{icecream[2]} {icecream[0]} and {name} choosees to be {icecream[1]}.''')
+
+
+if __name__ == "__main__":
+    main()
