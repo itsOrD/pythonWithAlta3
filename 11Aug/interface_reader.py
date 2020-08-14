30e009eeddbcd17ccec69dbb6662c02e8ebff5ea
Matthew Beckerleg <matthew.beckerleg@gmail.com>
2020-08-14 09:43:42 -0700

    interface reader practice

--- 11Aug/interface_reader.py
+++ 11Aug/interface_reader.py
@@ -1,3 +1,19 @@
 #!/usr/bin/python3
-'''itsOrD||netinterface'''
+''' itsOrD || netinterface '''
 
+
+import netifaces
+
+
+def main():
+    # playing with networking in python on the ipad
+    print(netifaces.interfaces())
+    print(netifaces.interfaces())
+
+    
+    for i in netifaces.interfaces():
+        print('\n**************Details of Interface - ' + i + ' *********************')
+        print(netifaces.ifaddresses(i))    
+    
+if __name__ == "__main__":
+    main()
