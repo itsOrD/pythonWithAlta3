1ff3ed7ffa61b043a609c6e042b04d7d0187d929
Matthew Beckerleg <matthew.beckerleg@gmail.com>
2020-08-18 11:01:51 -0700

    skeleton of personal flask-api sessions+login and example file 04

---
+++ 18Aug/flaskapi.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+''' itsOrD || using cookies with Flask '''
+
+
+from flask import Flask, session, render_template, redirect, url_for, escape, request
+
+app = flask(__name__)
+app.secret_key = "randomstr"
+
+# if the user...
+
+
+def main():
+    # basic user login protection
+    
+
+@app.route('/')
+def login():
+    # home page is login page
+    
+    
+    
+if __name__ == "__main__":
+    main()
